4a95d97b199dbcf360bcc7156513fe164d9d69df
Anna Hogberg <am.hogberg@gmail.com>
2018-07-16 12:45:57 +0200

    User model

--- models/User.js
+++ models/User.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    address: {type: String},
+    rate: {type: Number},
+    hours: { type: String, enum: ["Morning", "Afternoon", "Always"]},
+    isToc: {type: Boolean, default: false},
+    availability: {type: Boolean, default: true},
+    fee: { type: Number, default: null }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
