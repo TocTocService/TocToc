224484b203938d27c8d9f43162da7657ab36d0d1
Anna Hogberg <am.hogberg@gmail.com>
2018-07-16 13:27:20 +0200

    models done

--- models/User.js
+++ models/User.js
@@ -7,19 +7,12 @@ const userSchema = new Schema(
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     address: {type: String},
-    rate: {type: Number},
     hours: { type: String, enum: ["Morning", "Afternoon", "Always"]},
     isToc: {type: Boolean, default: false},
     availability: {type: Boolean, default: true},
     fee: { type: Number, default: null }
-  },
-  {
-    timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
-    }
-  }
-);
+  });
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
