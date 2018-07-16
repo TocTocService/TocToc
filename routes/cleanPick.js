52100d32d6698dc1d066a2281a49b3b403a048fc
Anna Hogberg <am.hogberg@gmail.com>
2018-07-16 20:11:31 +0200

    pickDateRouter half way

--- routes/cleanPick.js
+++ routes/cleanPick.js
@@ -12,6 +12,67 @@ cleanPickRoutes.use((req, res, next) => {
 });
 
 
+cleanPickRoutes.get('/profile', (req, res, next) => {
+  let query;
+  
+  if (req.user.isToc) {
+    query = { cleaner: req.user._id };
+  } else {
+    query = { user: req.user._id };
+  }
+  
+  PickDate
+  .find(query)
+  .populate('user', 'name')
+  .populate('cleaner', 'name')
+  .sort('serviceDate')
+  .exec((err, serviceDocs) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    
+    res.render('/profile', {
+      pickups: serviceDocs
+    });
+  });
+});
 
+cleanPickRoutes.get('/cleaners', (req, res, next) => {
+  User.find({ isToc: true }, (err, cleanerList) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('userpage/tocCleaners', {
+      cleaners: cleanerList
+    });
+  });
+});
+
+cleanPickRoutes.post('/cleaners', (req, res, next) => {
+  const userId = req.user._id;
+  const cleanerInfo = {
+    fee: req.body.fee,
+    isToc: true
+  }
+  User.findByIdAndUpdate(userId, cleanerInfo, { new: true }, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    req.user = theUser;
+
+    res.redirect('/profile');
+  });
+})
+
+
+
+cleanPickRoutes.get("/scheduleService/:id", (req, res, next) => {
+  res.render("userpage/cleaner-profile", { "message": req.flash("error") });
+});
 
 module.exports = cleanPickRoutes;
